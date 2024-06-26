9f43c8ef95b80b61465d0651541f75a9a0b2f350
Drew Edgar <2647430E@student.gla.ac.uk>
2024-06-26 22:02:47 +0100

    Restructured functions and fixed error handling
    - Moved helper functions outside of main
    - Fixed error handling so corrupted Balatro installs on MacOS will fail

--- main.py
+++ main.py
@@ -1,78 +1,81 @@
-def main():
-    import subprocess
-    import os
-    import urllib.request
-    import shutil
-    import platform
+import subprocess
+import os
+import urllib.request
+import shutil
+import platform
 
-    os_platform = platform.system()
+def look_for_balatro_windows() -> str:
+    path = "C:/Program Files (x86)/Steam/steamapps/common/Balatro/Balatro.exe"
+    does_balatro_exist = os.path.isfile(path)
+    if does_balatro_exist:
+        print("Balatro found! " + path)
+    else:
+        print("Balatro not found in default path!")
+    
+    while not does_balatro_exist:
+        path = input("Enter the file path of Balatro (including /Balatro.exe at the end): ")
+        path = path.removeprefix("\"").removesuffix("\"")
 
-    sevenzip_download = "https://www.7-zip.org/a/7z1900-x64.exe"
-    def look_for_balatro_windows():
-        path = "C:/Program Files (x86)/Steam/steamapps/common/Balatro/Balatro.exe"
         does_balatro_exist = os.path.isfile(path)
         if does_balatro_exist:
             print("Balatro found! " + path)
         else:
-            print("Balatro not found in default path!")
-        
-        while not does_balatro_exist:
-            path = input("Enter the file path of Balatro (including /Balatro.exe at the end): ")
-            path = path.removeprefix("\"").removesuffix("\"")
+            print("Balatro not found in the path you entered!")
 
-            does_balatro_exist = os.path.isfile(path)
-            if does_balatro_exist:
-                print("Balatro found! " + path)
-            else:
-                print("Balatro not found in the path you entered!")
+    return os.path.realpath(path)
 
-        return os.path.realpath(path)
+def look_for_balatro_macos() -> str:
+    home_dir = os.path.expanduser("~")
+    relative_path = "Library/Application Support/Steam/steamapps/common/Balatro/Balatro.app"
+    path = os.path.join(home_dir, relative_path)
+    does_balatro_exist = os.path.exists(path) and os.path.isdir(path)
+    if does_balatro_exist:
+        print("Balatro found! " + path)
+    else:
+        print("Balatro not found in default path!")
+    
+    while not does_balatro_exist:
+        path = input("Enter the file path of Balatro (including /Balatro.app at the end): ")
+        path = path.removeprefix("\"").removesuffix("\"")
 
-    def look_for_balatro_macos():
-        home_dir = os.path.expanduser("~")
-        relative_path = "Library/Application Support/Steam/steamapps/common/Balatro/Balatro.app"
-        path = os.path.join(home_dir, relative_path)
         does_balatro_exist = os.path.exists(path) and os.path.isdir(path)
         if does_balatro_exist:
             print("Balatro found! " + path)
         else:
-            print("Balatro not found in default path!")
-        
-        while not does_balatro_exist:
-            path = input("Enter the file path of Balatro (including /Balatro.app at the end): ")
-            path = path.removeprefix("\"").removesuffix("\"")
+            print("Balatro not found in the path you entered!")
 
-            does_balatro_exist = os.path.exists(path) and os.path.isdir(path)
-            if does_balatro_exist:
-                print("Balatro found! " + path)
-            else:
-                print("Balatro not found in the path you entered!")
+    path = os.path.join(path, "Contents/Resources/Balatro.love")
+    if not os.path.isfile(path):
+        print("Balatro directory corrupted")
+        return ""
+    return os.path.realpath(path)
 
-        path = os.path.join(path, "Contents/Resources/Balatro.love")
-        if not os.path.isfile(path):
-            print("Balatro directory corrupted, exiting...")
-            return
-        return os.path.realpath(path)
+def check_7zip():
+    sevenzip_download = "https://www.7-zip.org/a/7z1900-x64.exe"
+    try:
+        os.system("\"C:/Program Files/7-Zip/7z.exe\"")
+        print("7zip found!")
+    except FileNotFoundError:
+        print("7zip not found! Installing 7zip...")
+        # download 7zip
+        urllib.request.urlretrieve(sevenzip_download, "7zip.exe")
+        print("We will now open the 7zip installer. You will be asked for admin permissions.")
+        input("Press Enter to continue...")
+        print("Installing 7zip...")
+        subprocess.call("7zip.exe /S /D=C:/Program Files/7-Zip/")
+        print("7zip installed!")
 
-    def check_7zip():
-        try:
-            os.system("\"C:/Program Files/7-Zip/7z.exe\"")
-            print("7zip found!")
-        except FileNotFoundError:
-            print("7zip not found! Installing 7zip...")
-            # download 7zip
-            urllib.request.urlretrieve(sevenzip_download, "7zip.exe")
-            print("We will now open the 7zip installer. You will be asked for admin permissions.")
-            input("Press Enter to continue...")
-            print("Installing 7zip...")
-            subprocess.call("7zip.exe /S /D=C:/Program Files/7-Zip/")
-            print("7zip installed!")
+def main():
+    os_platform = platform.system()
 
     if os_platform == "Windows":    
         path = look_for_balatro_windows()
         check_7zip()
     elif os_platform == "Darwin":
         path = look_for_balatro_macos()
+        if (path == ""):
+            print("exiting...")
+            return
     # check if 7zip is installed
 
 
